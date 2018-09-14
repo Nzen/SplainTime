8d79e6d9cdd66adfccadb2104b132f8d20c04830
Nicholas Prado <nmprado@nzen.ws>
2018-09-13 22:10:26 -0500

    Ah, modules export packages, not modules

---
+++ src/main/module-info.java
@@ -0,0 +1,9 @@
+/** see ../../../../../LICENSE for release details */
+/**  */
+module ws.nzen.schedule.splaintime
+{
+	exports ws.nzen.splaintime;
+	exports ws.nzen.splaintime.model;
+
+	requires java.desktop;
+}
