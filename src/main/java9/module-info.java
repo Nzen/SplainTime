9da0ca4d42ee082f87fc826eae435dfc0db309c9
Nicholas Prado <nick@getworkflo.com>
2020-02-14 18:55:08 -0600

    fix i700101 ineffective attempt at multirelease jar
    Imitates Lanterna's multi compile pom. It's not yet clear why this
    doesn't compile the module-info. It also moves jooq generation
    to a non-default profile so new developers aren't hosed when
    maven helpfully imitates the existing empty database and removes
    the jooq files to match.

---
+++ src/main/java9/module-info.java
@@ -0,0 +1,11 @@
+/** see ../../../../../LICENSE for release details */
+/**  */
+module ws.nzen.schedule.splaintime
+{
+	exports ws.nzen.tracking.splaintime;
+	exports ws.nzen.tracking.splaintime.model;
+	exports ws.nzen.tracking.splaintime.dao.jooq;
+
+	requires java.desktop;
+}
+
